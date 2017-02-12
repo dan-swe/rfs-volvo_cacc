d345d03cff5aec0fc88ad4c6a5d866cd3c3b2910
John Spring <jspring@berkeley.edu>
2017-02-12 06:01:13 -0500

    modified:   long_ctl/src/avcs/run_can1_only.sh modified:   long_ctl/src/avcs/run_can2_only.sh modified:   test/volvo.sm Added tests for CAN1 and DVI to volvo.sm Changed run_can1_only and run_can2_only to look in relative paths for db_slv, etc., instead of absolute paths, because volvo.sm is sometimes not in /home/truckcontrol/test

--- truckcontrol/long_ctl/src/avcs/run_can2_only.sh
+++ truckcontrol/long_ctl/src/avcs/run_can2_only.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
+TEST_DIR='../../../test'
+CAN_DIR='../../../../can'
+
 slay  -f -Q -s TERM long_trk
+slay  -f -Q -s TERM dvi_rcv
+slay  -f -Q -s TERM dvi_snd
+slay  -f -Q -s TERM veh_snd
+slay  -f -Q -s TERM veh_rcv
 slay  -f -Q -s TERM trk_wr
 slay  -f -Q -s TERM rdj1939
 slay  -f -Q -s TERM gpsdb
+slay  -f -Q -s TERM jbussendGPS
+slay  -f -Q -s TERM jbussend
 slay  -f -Q -s TERM can_man
 slay  -f -Q -s TERM trk_cr
 slay  -f -Q -s TERM db_slv
 
-DATESTR=`date +%m%d%Y_%H%M%S`
-echo $DATESTR >/big/data/last_start_timestamp.txt
-/home/truckcontrol/test/db_slv -Q -S `hostname` &
+$TEST_DIR/db_slv -Q -S `hostname` &
 sleep 2
-/home/truckcontrol/test/trk_cr -t 1000 2>long_input$DATESTR.dbg &
+$TEST_DIR/trk_cr -t 1000 2>long_input$DATESTR.dbg &
 sleep 2
-/home/can/drv_sja1000/qnx/can_man -n /dev/can2 -s 250 -i 7 -p 0xda200 -e 1 &
+$CAN_DIR/drv_sja1000/qnx/can_man -n /dev/can2 -s 250 -i 7 -p 0xda200 -e 1 &
 sleep 2
-/home/truckcontrol/test/gpsdb -n 202 -d1 </dev/ser1 &
-sleep 2
-/home/can/jbus/src/qnx/rdj1939 -v -c -f /dev/can2
+$CAN_DIR/jbus/src/qnx/rdj1939 -v -c -f /dev/can2
+#$CAN_DIR/jbus/src/qnx/rdj1939 -v -g -c -f /dev/can2
